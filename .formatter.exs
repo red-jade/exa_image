d88f3beb3bcd8e972c95d901d0e06019e8e2c1cc
red-jade <katagorikal@gmail.com>
2024-08-26 13:06:23 +0700

    Latest exa mix util
    Ignore mix.lock

--- .formatter.exs
+++ .formatter.exs
@@ -4,10 +4,5 @@
     Enum.flat_map(
       ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
       &Path.wildcard(&1, match_dot: true)
-    ) --
-      [
-        "lib/gfx/space/xform.ex",
-        "lib/gis/location.ex",
-        "test/exa/image/bitmap_test.exs"
-      ]
+    ) -- ["test/exa/image/bitmap_test.exs"]
 ]
